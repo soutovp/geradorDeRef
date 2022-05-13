293b324374baff05ae984a2566da477692b27b77
soutovp <fernandoandradesouto@hotmail.com>
2022-05-13 10:00:22 -0300

    Update: Adicionado possibilidade de entrada de referências dez mil

--- js/script.js
+++ js/script.js
@@ -4,49 +4,52 @@ function limpar(){
 function listar(){
     var t = document.getElementById("referencia").value.toUpperCase();
     var resultado = document.getElementById("resultado");
-    if(document.getElementsByClassName("numeros").value = ""){
-        document.getElementsByClassName("numeros").value = 0;
-    }
-    for(let i = 0; i<document.getElementById("33").value; i++){
-        resultado.innerHTML += t+".N33<br>";
-    }
-    for(let i = 0; i<document.getElementById("34").value; i++){
-        resultado.innerHTML += t+".N34<br>";
-    }
-    for(let i = 0; i<document.getElementById("35").value; i++){
-        resultado.innerHTML += t+".N35<br>";
-    }
-    for(let i = 0; i<document.getElementById("36").value; i++){
-        resultado.innerHTML += t+".N36<br>";
-    }
-    for(let i = 0; i<document.getElementById("37").value; i++){
-        resultado.innerHTML += t+".N37<br>";
-    }
-    for(let i = 0; i<document.getElementById("38").value; i++){
-        resultado.innerHTML += t+".N38<br>";
-    }
-    for(let i = 0; i<document.getElementById("39").value; i++){
-        resultado.innerHTML += t+".N39<br>";
-    }
-    for(let i = 0; i<document.getElementById("40").value; i++){
-        resultado.innerHTML += t+".N40<br>";
-    }
-    for(let i = 0; i<document.getElementById("41").value; i++){
-        resultado.innerHTML += t+".N41<br>";
-    }
-    for(let i = 0; i<document.getElementById("42").value; i++){
-        resultado.innerHTML += t+".N42<br>";
-    }
-    for(let i = 0; i<document.getElementById("43").value; i++){
-        resultado.innerHTML += t+".N43<br>";
-    }
-    for(let i = 0; i<document.getElementById("44").value; i++){
-        resultado.innerHTML += t+".N44<br>";
-    }
-    for(let i = 0; i<document.getElementById("45").value; i++){
-        resultado.innerHTML += t+".N45<br>";
-    }
-    for(let i = 0; i<document.getElementById("uni").value; i++){
-        resultado.innerHTML += t+".UNI<br>";
+    if(document.getElementById("manualCheck").checked != true){
+        for(let i = 0; i<document.getElementById("33").value; i++){
+            resultado.innerHTML += t+".N33<br>";
+        }
+        for(let i = 0; i<document.getElementById("34").value; i++){
+            resultado.innerHTML += t+".N34<br>";
+        }
+        for(let i = 0; i<document.getElementById("35").value; i++){
+            resultado.innerHTML += t+".N35<br>";
+        }
+        for(let i = 0; i<document.getElementById("36").value; i++){
+            resultado.innerHTML += t+".N36<br>";
+        }
+        for(let i = 0; i<document.getElementById("37").value; i++){
+            resultado.innerHTML += t+".N37<br>";
+        }
+        for(let i = 0; i<document.getElementById("38").value; i++){
+            resultado.innerHTML += t+".N38<br>";
+        }
+        for(let i = 0; i<document.getElementById("39").value; i++){
+            resultado.innerHTML += t+".N39<br>";
+        }
+        for(let i = 0; i<document.getElementById("40").value; i++){
+            resultado.innerHTML += t+".N40<br>";
+        }
+        for(let i = 0; i<document.getElementById("41").value; i++){
+            resultado.innerHTML += t+".N41<br>";
+        }
+        for(let i = 0; i<document.getElementById("42").value; i++){
+            resultado.innerHTML += t+".N42<br>";
+        }
+        for(let i = 0; i<document.getElementById("43").value; i++){
+            resultado.innerHTML += t+".N43<br>";
+        }
+        for(let i = 0; i<document.getElementById("44").value; i++){
+            resultado.innerHTML += t+".N44<br>";
+        }
+        for(let i = 0; i<document.getElementById("45").value; i++){
+            resultado.innerHTML += t+".N45<br>";
+        }
+        for(let i = 0; i<document.getElementById("uni").value; i++){
+            resultado.innerHTML += t+".UNI<br>";
+        }
+    }else{
+        for(let i = 0; i<document.getElementById("manual").value; i++){
+            resultado.innerHTML += t+"<br>";
+        }
     }
 }
