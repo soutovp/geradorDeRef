7b3e2be385955e25c6b90c49149c02be859e7dfe
soutovp <fernandoandradesouto@hotmail.com>
2022-05-13 09:43:03 -0300

    Modelo Gerador de Listagem 1.0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
-alert('Hello World!');
+function limpar(){
+    document.getElementById("resultado").innerHTML = "";
+}
+function listar(){
+    var t = document.getElementById("referencia").value.toUpperCase();
+    var resultado = document.getElementById("resultado");
+    if(document.getElementsByClassName("numeros").value = ""){
+        document.getElementsByClassName("numeros").value = 0;
+    }
+    for(let i = 0; i<document.getElementById("33").value; i++){
+        resultado.innerHTML += t+".N33<br>";
+    }
+    for(let i = 0; i<document.getElementById("34").value; i++){
+        resultado.innerHTML += t+".N34<br>";
+    }
+    for(let i = 0; i<document.getElementById("35").value; i++){
+        resultado.innerHTML += t+".N35<br>";
+    }
+    for(let i = 0; i<document.getElementById("36").value; i++){
+        resultado.innerHTML += t+".N36<br>";
+    }
+    for(let i = 0; i<document.getElementById("37").value; i++){
+        resultado.innerHTML += t+".N37<br>";
+    }
+    for(let i = 0; i<document.getElementById("38").value; i++){
+        resultado.innerHTML += t+".N38<br>";
+    }
+    for(let i = 0; i<document.getElementById("39").value; i++){
+        resultado.innerHTML += t+".N39<br>";
+    }
+    for(let i = 0; i<document.getElementById("40").value; i++){
+        resultado.innerHTML += t+".N40<br>";
+    }
+    for(let i = 0; i<document.getElementById("41").value; i++){
+        resultado.innerHTML += t+".N41<br>";
+    }
+    for(let i = 0; i<document.getElementById("42").value; i++){
+        resultado.innerHTML += t+".N42<br>";
+    }
+    for(let i = 0; i<document.getElementById("43").value; i++){
+        resultado.innerHTML += t+".N43<br>";
+    }
+    for(let i = 0; i<document.getElementById("44").value; i++){
+        resultado.innerHTML += t+".N44<br>";
+    }
+    for(let i = 0; i<document.getElementById("45").value; i++){
+        resultado.innerHTML += t+".N45<br>";
+    }
+    for(let i = 0; i<document.getElementById("uni").value; i++){
+        resultado.innerHTML += t+".UNI<br>";
+    }
+}
