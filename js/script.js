5ebd864f75dde816f64bbb556cb63e4c3ba038e1
soutovp <fernandoandradesouto@hotmail.com>
2022-05-14 12:06:35 -0300

    Update : Melhora do código Javascript / HTML

--- js/script.js
+++ js/script.js
@@ -1,60 +1,48 @@
+function destaque(){
+    if(document.getElementById("resultado").innerText != ""){
+        document.getElementById("btnCopy").className = "destacado";
+    }else{
+        document.getElementById("btnCopy").className = "btnDesativado"
+    }
+}
 function copiar(){
-    let text = document.getElementById("resultado").innerText;
-    navigator.clipboard.writeText(text);
-    alert("Texto Copiado!");
+    if(document.getElementById("resultado").innerText != ""){
+        let text = document.getElementById("resultado").innerText;
+        navigator.clipboard.writeText(text);
+        alert("Texto Copiado!");
+    }
 }
 function limpar(){
     document.getElementById("resultado").innerHTML = "";
+    destaque();
 }
 function listar(){
-    var t = document.getElementById("referencia").value.toUpperCase();
     var resultado = document.getElementById("resultado");
+    var referencia = document.getElementById("referencia").value.toUpperCase();
+    function valor(val){
+        return document.getElementById(val).value;
+    }
+    let numeracoes = [33,34,35,36,37,38,39,40,41,42,43,44,45,"uni","manual"]
     if(document.getElementById("manualCheck").checked != true){
-        for(let i = 0; i<document.getElementById("33").value; i++){
-            resultado.innerHTML += t+".N33<br>";
+        var anonimo = function(valor, num){
+            if(valor>500){
+                alert("Não é permitido valores maiores que 500");
+            }else{
+                for(let i = 0; i<valor && valor<=100; i++){
+                    resultado.innerHTML += referencia+".N"+num+"<br>";
+                }
+            }
         }
-        for(let i = 0; i<document.getElementById("34").value; i++){
-            resultado.innerHTML += t+".N34<br>";
-        }
-        for(let i = 0; i<document.getElementById("35").value; i++){
-            resultado.innerHTML += t+".N35<br>";
-        }
-        for(let i = 0; i<document.getElementById("36").value; i++){
-            resultado.innerHTML += t+".N36<br>";
-        }
-        for(let i = 0; i<document.getElementById("37").value; i++){
-            resultado.innerHTML += t+".N37<br>";
-        }
-        for(let i = 0; i<document.getElementById("38").value; i++){
-            resultado.innerHTML += t+".N38<br>";
-        }
-        for(let i = 0; i<document.getElementById("39").value; i++){
-            resultado.innerHTML += t+".N39<br>";
-        }
-        for(let i = 0; i<document.getElementById("40").value; i++){
-            resultado.innerHTML += t+".N40<br>";
-        }
-        for(let i = 0; i<document.getElementById("41").value; i++){
-            resultado.innerHTML += t+".N41<br>";
-        }
-        for(let i = 0; i<document.getElementById("42").value; i++){
-            resultado.innerHTML += t+".N42<br>";
-        }
-        for(let i = 0; i<document.getElementById("43").value; i++){
-            resultado.innerHTML += t+".N43<br>";
-        }
-        for(let i = 0; i<document.getElementById("44").value; i++){
-            resultado.innerHTML += t+".N44<br>";
-        }
-        for(let i = 0; i<document.getElementById("45").value; i++){
-            resultado.innerHTML += t+".N45<br>";
+        for(let i = 0; i<numeracoes.length-1;i++){
+            anonimo(valor(numeracoes[i]),numeracoes[i]);
         }
         for(let i = 0; i<document.getElementById("uni").value; i++){
-            resultado.innerHTML += t+".UNI<br>";
+            resultado.innerHTML += referencia+".UNI<br>";
         }
     }else{
         for(let i = 0; i<document.getElementById("manual").value; i++){
-            resultado.innerHTML += t+"<br>";
+            resultado.innerHTML += referencia+"<br>";
         }
     }
+    destaque();
 }
